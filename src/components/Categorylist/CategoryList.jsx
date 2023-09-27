3bc5273284b4f64392d0c6f73755c53ff4ed5b34
Rabeet-5 <rabeetsati@gmail.com>
2023-09-28 00:38:06 +0500

    blog categories + featured

--- src/components/Categorylist/CategoryList.jsx
+++ src/components/Categorylist/CategoryList.jsx
@@ -1,12 +1,70 @@
-import React from 'react'
-import styles from './categoryList.module.css'
+import React from "react";
+import styles from "./categoryList.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const CategoryList = () => {
   return (
     <div className={styles.container}>
-      CategoryList
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+          <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+            <Image
+              src="/style.png"
+              width={32}
+              height={32}
+              className={styles.image}
+            />
+            Style
+          </Link>
+          <Link href="/blog" className={`${styles.category} ${styles.fashion}`}>
+            <Image
+              src="/fashion.png"
+              width={32}
+              height={32}
+              className={styles.image}
+            />
+            Fashion
+          </Link>
+          <Link href="/blog" className={`${styles.category} ${styles.coding}`}>
+            <Image
+              src="/coding.png"
+              width={32}
+              height={32}
+              className={styles.image}
+            />
+            Coding
+          </Link>
+          <Link href="/blog" className={`${styles.category} ${styles.food}`}>
+            <Image
+              src="/food.png"
+              width={32}
+              height={32}
+              className={styles.image}
+            />
+            Food
+          </Link>
+          <Link href="/blog" className={`${styles.category} ${styles.fashion}`}>
+            <Image
+              src="/fashion.png"
+              width={32}
+              height={32}
+              className={styles.image}
+            />
+            Travel
+          </Link>
+          <Link href="/blog" className={`${styles.category} ${styles.culture}`}>
+            <Image
+              src="/fashion.png"
+              width={32}
+              height={32}
+              className={styles.image}
+            />
+            Culture
+          </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CategoryList
+export default CategoryList;
--- src/components/feataured/Featured.jsx
+++ src/components/feataured/Featured.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import styles from './featured.module.css'
+import React from "react";
+import styles from "./featured.module.css";
+import Image from "next/image";
 
 const Featured = () => {
   return (
     <div className={styles.container}>
-      Featured
+      <h1 className={styles.title}>
+        Hey !<b> Rabeet Sati Here </b> Discover my blog's and stories
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="post" fill />
+        </div>
+        <div className={styles.textContainer}>
+          <h2 className={styles.postTitle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+          </h2>
+          <p className={styles.postDescription}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+            esse illo, sint similique tempora voluptatum rem dolor cumque
+            reiciendis corrupti quasi harum a quam blanditiis omnis excepturi
+            eligendi cum maiores.
+          </p>
+          <button className={styles.button}>Read more</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Featured
+export default Featured;
