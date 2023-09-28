2c2be35ecad8414c62c1546af69b67f171e20157
Rabeet-5 <rabeetsati@gmail.com>
2023-09-28 22:21:44 +0500

    commit

---
+++ src/app/[slug]/page.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./singlepage.module.css";
+import Menu from "@/components/menu/Menu";
+import Image from "next/image";
+
+const SinglePage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.infoContainer}>
+        <div className={styles.textContainer}>
+          <h1>React Js Coding</h1>
+          <div className={styles.user}>
+            <div className={styles.userImageContainer}>
+              <Image src="/p1.jpeg" alt="postImage" fill className={styles.image} />
+            </div>
+            <div className={styles.userTextContainer}>
+                <span className={styles.username}>Rabeet sati</span>
+                <span className={styles.date}>25-09-2023</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image src="/p1.jpeg" alt="postImage" fill className={styles.image} />
+        </div>
+      </div>
+
+      <div className={styles.content}>
+        <div className={styles.post}></div>
+        <Menu />
+      </div>
+    </div>
+  );
+};
+
+export default SinglePage;
