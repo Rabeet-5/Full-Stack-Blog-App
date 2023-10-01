c16272e0cb6a46ff502d023c5fa6343be063e9fa
Rabeet-5 <rabeetsati@gmail.com>
2023-10-01 17:22:04 +0500

    commit

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./comments.module.css";
 import Link from "next/link";
 import Image from "next/image";
@@ -20,20 +20,35 @@ const fetcher = async (url) => {
 };
 
 const Comments = ({ postSlug }) => {
-  const {status} = useSession();
+  const { status } = useSession();
   console.log(postSlug);
-  const { data, isLoading } = useSWR(
+  const { data, mutate, isLoading } = useSWR(
     `http://localhost:3000/api/comment?postSlug=${postSlug}`,
     fetcher
   );
+
+  const [desc, setDesc] = useState("");
+  const handleSubmit = async () => {
+    await fetch("/api/comment", {
+      method: "POST",
+      body: JSON.stringify({ desc, postSlug }),
+    });
+    mutate()
+  };
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Comments</h1>
       {status === "authenticated" ? (
         <div className={styles.write}>
-          <textarea placeholder="Write a comment..." className={styles.input} />
-          <button className={styles.button}>Send</button>
+          <textarea
+            placeholder="Write a comment..."
+            className={styles.input}
+            onChange={(e) => setDesc(e.target.value)}
+          />
+          <button className={styles.button} onClick={handleSubmit}>
+            Send
+          </button>
         </div>
       ) : (
         <Link href="/login">Login to write a comment</Link>
@@ -44,21 +59,21 @@ const Comments = ({ postSlug }) => {
           : data?.map((item) => (
               <div className={styles.comment} key={item._id}>
                 <div className={styles.user}>
-                  {item.user.image && <Image
-                    src={item.user.image}
-                    alt="post"
-                    width={50}
-                    height={50}
-                    className={styles.image}
-                  />}
+                  {item.user.image && (
+                    <Image
+                      src={item.user.image}
+                      alt="post"
+                      width={50}
+                      height={50}
+                      className={styles.image}
+                    />
+                  )}
                   <div className={styles.userInfo}>
                     <span className={styles.username}>{item.user.name}</span>
                     <span className={styles.date}>{item.createdAt}</span>
                   </div>
                 </div>
-                <p className={styles.desc}>
-                  {item.desc}
-                </p>
+                <p className={styles.desc}>{item.desc}</p>
               </div>
             ))}
       </div>
