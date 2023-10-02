28f0dbb3b5d0af24a0439589b8feea9950ff2ad6
Rabeet-5 <rabeetsati@gmail.com>
2023-10-03 01:11:40 +0500

    commit

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./login.module.css";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
@@ -7,23 +7,23 @@ import toast, { Toaster } from "react-hot-toast";
 
 const Page = () => {
   const { data, status } = useSession();
-  const router = useRouter()
-  console.log(data, status);
+  const router = useRouter();
 
+  useEffect(() => {
+    if (status === 'loading') {
+    } else if (status === 'authenticated') {
+      toast.success('Login Successful !');
+      router.push('/');
+    } else {
+    }
+  }, [status, router]);
 
-  if(status === 'loading'){
-    return <div className={styles.loading}>loading....</div>
-  }
-  if(status === 'authenticated'){
-    toast.success(' Login Successfull !')
-    router.push('/')
-  }
   return (
     <>
       <div className={styles.container}>
         <div className={styles.wrapper}>
-          <div className={styles.socialButton} onClick={()=>signIn('google')}>Sign in with Google</div>
-         <Toaster />
+          <div className={styles.socialButton} onClick={() => signIn('google')}>Sign in with Google</div>
+          <Toaster />
         </div>
       </div>
     </>
